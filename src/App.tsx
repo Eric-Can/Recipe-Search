3553201a78fdc3f05212712fc8ab658e7fa57a71
Eric-Can <canerse.ec@gmail.com>
2021-05-19 06:25:04 -0500

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import Reactfrom, { StrictMode } from "react";
+import ReactDOM, { render } from "react-dom";
+
+const App = () => {
+  return <div>Hello World!</div>;
+};
+
+render(<App />, document.getElementById("root"));
